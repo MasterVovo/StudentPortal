9ef6a3906c37e7901a43ad7f7eca440dc4885b0d
MasterVovo <johnandrewreyes3@gmail.com>
2023-10-09 16:44:37 +0800

    Dashboard na medyo pinaganda ng slight

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,24 @@
+const sidebar = document.querySelector('aside');
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+
+const darkMode = document.querySelector('.dark-mode');
+
+menuBtn.addEventListener('click', () => {
+    sidebar.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', () => {
+    sidebar.style.display = 'none';
+});
+
+darkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+
+// For the popu logout
 const logoutBtn = document.getElementById('logoutBtn');
 const confirmationDialog = document.getElementById('confirmationDialog');
 const confirmLogout = document.getElementById('confirmLogout');
