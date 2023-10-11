24a8826c840d53584ceb503d674bad35503cb3df
MasterVovo <johnandrewreyes3@gmail.com>
2023-10-11 22:00:24 +0800

    Finished the layout, lacking responsiveness still

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -16,35 +16,11 @@ darkMode.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode-variables');
     darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
     darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
-})
-
-// For the popu logout
-const logoutBtn = document.getElementById('logoutBtn');
-const confirmationDialog = document.getElementById('confirmationDialog');
-const confirmLogout = document.getElementById('confirmLogout');
-const cancelLogout = document.getElementById('cancelLogout');
-
-logoutBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    confirmationDialog.classList.add('show');
 });
 
-confirmLogout.addEventListener('click', (e) => {
-    e.preventDefault();
-    window.location.href = 'PageNotFound.html';
-});
-
-cancelLogout.addEventListener('click', (e) => {
-    e.preventDefault();
-    confirmationDialog.classList.add('hide');
-    setTimeout(() => {
-        confirmationDialog.classList.remove('show');
-        confirmationDialog.classList.remove('hide');
-    }, 300);
-});
 
 const toggler = document.querySelector('.btn');
 
 toggler.addEventListener('click', function(){
     document.querySelector('#sidebar').classList.toggle('collapsed');
-})
+});
