1c5a5ddbc1d7990539faecc841af3efaed24c647
JhnlldMata <johnlloydmata9@gmail.com>
2023-10-12 18:51:01 +0800

    Tapos na din sa wakas, sa ngayon

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,32 +1,40 @@
 const sidebar = document.querySelector('aside');
 const menuBtn = document.getElementById('menu-btn');
 const closeBtn = document.getElementById('close-btn');
-const darkMode = document.querySelector('.dark-mode');
-const toggler = document.querySelector('.btn');
 
-// Check if dark mode preference is already stored in local storage
-const darkModePreference = localStorage.getItem('darkMode');
-
-// Set dark mode based on the stored preference
-if (darkModePreference === 'true') {
-  document.body.classList.add('dark-mode-variables');
-  darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
-  darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
-}
-
-// Event listener for the dark mode toggle button
-darkMode.addEventListener('click', () => {
-  // Toggle dark mode class on the body element
-  document.body.classList.toggle('dark-mode-variables');
-  darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
-  darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
-
-  // Update the dark mode preference in local storage
-  const isDarkMode = document.body.classList.contains('dark-mode-variables');
-  localStorage.setItem('darkMode', isDarkMode.toString());
+menuBtn.addEventListener('click', () => {// open sidebar
+    sidebar.style.display = 'block';
+    sidebar.style.animation = 'showMenu 0.5s ease-in-out forwards';
 });
 
-// Event listener for the toggler button
-toggler.addEventListener('click', () => {
-  document.querySelector('#sidebar').classList.toggle('collapsed');
+closeBtn.addEventListener('click', () => {// close sidebar
+    sidebar.style.animation = 'hideMenu 0.5s ease-in-out forwards';
+    
+    setTimeout(() => { // hide sidebar after 300ms
+        sidebar.style.display = 'none';
+    }, 300);
+});
+
+
+const darkMode = document.querySelector('.dark-mode');
+const toggler = document.querySelector('.btn');
+const darkModePreference = localStorage.getItem('darkMode');// store dark mode preference
+
+if (darkModePreference === 'true') { // if dark mode preference is true
+    document.body.classList.add('dark-mode-variables'); // apply dark mode
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+}
+
+darkMode.addEventListener('click', () => { // toggle dark mode
+    document.body.classList.toggle('dark-mode-variables');
+    darkMode.querySelector('span:nth-child(1)').classList.toggle('active');
+    darkMode.querySelector('span:nth-child(2)').classList.toggle('active');
+
+    const isDarkMode = document.body.classList.contains('dark-mode-variables');
+    localStorage.setItem('darkMode', isDarkMode.toString()); 
+});
+
+toggler.addEventListener('click', () => { // toggle sidebar
+    document.querySelector('#sidebar').classList.toggle('collapsed');
 });
