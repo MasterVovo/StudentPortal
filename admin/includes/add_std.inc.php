f749d4f22533f86c35aef6a6ae0c85e875798ca2
AibuKuan <kuanaibu369@gmail.com>
2023-11-23 19:02:17 +0800

    Fixed console errors

--- admin/includes/add_std.inc.php
+++ admin/includes/add_std.inc.php
@@ -3,7 +3,28 @@ require "../../sqlConnection/db_connect.php";
 require "generate-pass.inc.php";
 require "send-email.inc.php";
 
+$uploadStatus = array();
+
 $jsonData = $_POST['jsonData'];
+// $jsonData = json_encode(array(
+//     array(
+//       "stdID" => 1,
+//       "stdFName" => "Jason",
+//       "stdMName" => "Foreman",
+//       "stdLName" => "Hello",
+//       "stdCourse" => "PSY",
+//       "stdEmail" => "ahtiw3ukkhtew@gmail.com"
+      
+//     ),
+//     array(
+//         "stdID" => 2,
+//         "stdFName" => "Thor",
+//         "stdMName" => "Foreman",
+//         "stdLName" => "Hello",
+//         "stdCourse" => "PSY",
+//         "stdEmail" => "ahtiw3ukkhtew@gmail.com"
+//     )
+//  ));
 
 // Decode the JSON data
 $dataArray = json_decode($jsonData, true);
@@ -25,29 +46,22 @@ foreach ($dataArray as $item) {
     $stmt->bind_param("ssssss", $stdID, $stdFName, $stdMName, $stdLName, $stdCourse, $stdEmail);
     $stmt->execute();
 
-    // Check if the operation was successful
+    // Check if the insert to stdinfo was successful
     if ($stmt->affected_rows > 0) {
         $stmt = $conn->prepare("INSERT INTO userinfo (schoolID, userPass, userType) VALUES (?, ?, ?)");
         $stmt->bind_param("sss", $stdID, $hashedPass, $userType);
         $stmt->execute();
         
-        // Check if the operation was successful
+        // Check if the insert to userinfo was successful
         if ($stmt->affected_rows > 0) {
             sendEmail($stdEmail, $stdID, $userPass, $stdFName);
-            // Echo a success message
-            http_response_code(200);
-            echo json_encode(['success' => true, 'message' => 'Data inserted successfully']);
+            $uploadStatus[] = array('success' => true, 'message' => $stdFName . '\'s Data inserted successfully.');
         } else {
-            // Echo an error message
-            http_response_code(400);
-            echo json_encode(['success' => false, 'message' => 'Error inserting to userinfo']);
+            $uploadStatus[] = array('success' => false, 'message' => $stdFName . '\'s Data cannot be inserted to userinfo.');
         }
     } else {
-        // Echo an error message
-        http_response_code(400);
-        echo json_encode(['success' => false, 'message' => 'Error inserting to stdinfo']);
+        $uploadStatus[] = array('success' => false, 'message' => $stdFName . '\'s Data connot be inserted to stdinfo.');
     }
-
-    
 }
 
+echo json_encode($uploadStatus);
