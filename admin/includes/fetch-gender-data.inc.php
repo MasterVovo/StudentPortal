84636c8ea27a88ad4e21fdc97cf4debc3e3f2d97
AibuKuan <kuanaibu369@gmail.com>
2023-11-26 17:38:42 +0800

    Fetches the student gender data

---
+++ admin/includes/fetch-gender-data.inc.php
@@ -0,0 +1,22 @@
+<?php
+require "../../sqlConnection/db_connect.php";
+
+$sql = "SELECT `stdGender` FROM stdinfo;";
+$result = mysqli_query($conn, $sql);
+$resultCheck = mysqli_num_rows($result);
+
+if ($resultCheck > 0) {
+    $maleCount = 0;
+    $femaleCount = 0;
+
+    while ($gender = mysqli_fetch_assoc($result)) {
+        if ($gender == 'Male') {
+            $maleCount++;
+        } else if ($gender == 'Female') {
+            $femaleCount++;
+        }
+    }
+    
+    $data = array($maleCount, $femaleCount);
+    echo json_encode($data);
+}
