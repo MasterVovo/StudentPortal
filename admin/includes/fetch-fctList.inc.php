dcd6e2887e5177bdccb928095b37069cc83daf5e
AibuKuan <kuanaibu369@gmail.com>
2023-11-29 16:21:49 +0800

    Fetches and display sections

--- admin/includes/fetch-fctList.inc.php
+++ admin/includes/fetch-fctList.inc.php
@@ -1,65 +1,111 @@
 <?php
 require "../../sqlConnection/db_connect.php";
 
-$sql = "SELECT * FROM thrinfo;";
-$result = mysqli_query($conn, $sql);
-$resultCheck = mysqli_num_rows($result);
-
-$arrayOfFctData = array();
-
-if ($resultCheck > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $fctData = new fctData();
-
-        $fctData->appendThrId($row['thrId']);
-        $fctData->appendThrFName($row['thrFName']);
-        $fctData->appendThrMName($row['thrMName']);
-        $fctData->appendThrLName($row['thrLName']);
-        $fctData->appendThrDept($row['thrDept']);
-
-        // Fetches the section of the teacher
-        $sqlSection = "SELECT `sectionName` FROM tblsections WHERE (sectionAdviserId = '" . $row['thrId'] . "')";
-        $resultSection = mysqli_query($conn, $sqlSection);
-
-        while ($rowSection = mysqli_fetch_assoc($resultSection)) {
-            $fctData->appendSectionName($rowSection['sectionName']);
-        }
-
-        array_push($arrayOfFctData, $fctData);
+if (isset($_POST['functionName'])) {
+    $function = $_POST['functionName'];
+    
+    switch($function) {
+        case 'fetchThrData':
+            echo fetchThrData();
+            break;
+        case 'fetchSections':
+            echo fetchSections();
+            break;
     }
-    echo json_encode($arrayOfFctData);
 }
 
-
-class fctData {
-    public $thrId;
-    public $thrFName;
-    public $thrMName;
-    public $thrLName;
-    public $thrDept;
-    public $sectionName;
-
-    function appendThrId($data) {
-        $this->thrId = $data;
+function fetchThrData() {
+    class fctData {
+        public $thrId;
+        public $thrFName;
+        public $thrMName;
+        public $thrLName;
+        public $thrDept;
+        public $sectionName;
+    
+        function appendThrId($data) {
+            $this->thrId = $data;
+        }
+    
+        function appendThrFName($data) {
+            $this->thrFName = $data;
+        }
+    
+        function appendThrMName($data) {
+            $this->thrMName = $data;
+        }
+    
+        function appendThrLName($data) {
+            $this->thrLName = $data;
+        }
+    
+        function appendThrDept($data) {
+            $this->thrDept = $data;
+        }
+    
+        function appendSectionName($data) {
+            $this->sectionName = $data;
+        }
     }
 
-    function appendThrFName($data) {
-        $this->thrFName = $data;
+    global $conn;
+    $sql = "SELECT * FROM thrinfo;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+    
+    $arrayOfFctData = array();
+    
+    if ($resultCheck > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            $fctData = new fctData();
+    
+            $fctData->appendThrId($row['thrId']);
+            $fctData->appendThrFName($row['thrFName']);
+            $fctData->appendThrMName($row['thrMName']);
+            $fctData->appendThrLName($row['thrLName']);
+            $fctData->appendThrDept($row['thrDept']);
+    
+            // Fetches the section of the teacher
+            $sqlSection = "SELECT `sectionName` FROM tblsections WHERE (sectionAdviserId = '" . $row['thrId'] . "')";
+            $resultSection = mysqli_query($conn, $sqlSection);
+    
+            while ($rowSection = mysqli_fetch_assoc($resultSection)) {
+                $fctData->appendSectionName($rowSection['sectionName']);
+            }
+    
+            array_push($arrayOfFctData, $fctData);
+        }
+        return json_encode($arrayOfFctData);
+    }
+    
+    
+    
+}
+
+function fetchSections() {
+    class sectionList {
+        public $value;
+        public $text;
+
+        function addSection($sectionName) {
+            $this->value = $sectionName;
+            $this->text = $sectionName;
+        }
     }
 
-    function appendThrMName($data) {
-        $this->thrMName = $data;
-    }
+    global $conn;
+    $sql = "SELECT `sectionName` FROM tblsections";
+    $result = mysqli_query($conn, $sql);
 
-    function appendThrLName($data) {
-        $this->thrLName = $data;
-    }
-
-    function appendThrDept($data) {
-        $this->thrDept = $data;
-    }
-
-    function appendSectionName($data) {
-        $this->sectionName = $data;
+    if (mysqli_num_rows($result) > 0) {
+        $arrayOfSectionList = array();
+        while ($section = mysqli_fetch_assoc($result)) {
+            $sectionList = new sectionList();
+            $sectionList->addSection($section['sectionName']);
+            array_push($arrayOfSectionList, $sectionList);
+        }
+        return json_encode($arrayOfSectionList);
+    } else {
+        return 'No sections found';
     }
 }
--- admin/scripts/fctList.js
+++ admin/scripts/fctList.js
@@ -1,85 +1,95 @@
-let grid = $('#grid-table').jsGrid({
-    width: "100%",
-    height: "auto",
+function createGridChart(sections) {
+    let grid = $('#grid-table').jsGrid({
+        width: "100%",
+        height: "auto",
+    
+        filtering: false,
+        inserting: true,
+        editing: true,
+        sorting: true,
+        paging: true,
+        autoload: true,
+        pageSize: 15,
+        pageButtonCount: 5,
+        // deleteConfirm: "Do you really want to delete data?",
+    
+        controller: {
+            loadData: function() {
+                return $.ajax({
+                    type: "POST",
+                    url: "includes/fetch-fctList.inc.php",
+                    data: { functionName: 'fetchThrData' },
+                    dataType: "json"
+                });
+            }
+            // ,
+            // deleteItem: function(item) {
+            //     return $.ajax({
+            //         type: "POST",
+            //         url: "includes/delete-stdList.inc.php",
+            //         data: item
+            //     })
+            // }
+        },
+    
+        fields: [
+            {
+                name: "thrId",
+                title: "Teacher ID",
+                type: "text",
+                validate: "required"
+            },
+            {
+                name: "thrFName",
+                title: "First Name",
+                type: "text",
+                validate: "required"
+            },
+            {
+                name: "thrMName",
+                title: "Middle Name",
+                type: "text"
+            },
+            {
+                name: "thrLName",
+                title: "Last Name",
+                type: "text",
+                validate: "required"
+            },
+            {
+                name: "thrDept",
+                title: "Department",
+                type: "text",
+                validate: "required"
+            },
+            {
+                name: "sectionName",
+                title: "Section",
+                type: "select",
+                items: sections,
+                valueField: "value",
+                textField: "text"
+            },
+            {
+                type: "control"
+            }
+        ]
+    }).data("JSGrid");
+    
+    // The minimum width of the table for responsiveness
+    $('.jsgrid-table').css('min-width', '1800px');    
+}
 
-    filtering: true,
-    // inserting: true,
-    editing: true,
-    sorting: true,
-    paging: true,
-    autoload: true,
-    pageSize: 15,
-    pageButtonCount: 5,
-    // deleteConfirm: "Do you really want to delete data?",
-
-    controller: {
-        loadData: function(filter) {
-            return $.ajax({
-                type: "POST",
-                url: "includes/fetch-fctList.inc.php",
-                data: filter,
-                dataType: "json"
-            });
-        }
-        // ,
-        // deleteItem: function(item) {
-        //     return $.ajax({
-        //         type: "POST",
-        //         url: "includes/delete-stdList.inc.php",
-        //         data: item
-        //     })
-        // }
+$.ajax({
+    url: 'includes/fetch-fctList.inc.php',
+    type: 'POST',
+    data: {functionName: 'fetchSections'},
+    success: function(data) {
+        console.log(data);
+        const sections = JSON.parse(data);
+        createGridChart(sections);
     },
-
-    fields: [
-        {
-            name: "thrId",
-            title: "Teacher ID",
-            type: "text",
-            validate: "required"
-        },
-        {
-            name: "thrFName",
-            title: "First Name",
-            type: "text",
-            validate: "required"
-        },
-        {
-            name: "thrMName",
-            title: "Middle Name",
-            type: "text"
-        },
-        {
-            name: "thrLName",
-            title: "Last Name",
-            type: "text",
-            validate: "required"
-        },
-        {
-            name: "thrDept",
-            title: "Department",
-            type: "text",
-            validate: "required"
-        },
-        {
-            name: "sectionName",
-            title: "Section",
-            type: "select",
-            items: [
-                { value: "CIS201", text: "BSIS 201" }, 
-                { value: "CIS202", text: "BSIS 202" },
-                { value: "CIS203", text: "BSIS 203" },
-                { value: "CIS204", text: "BSIS 204" },
-                { value: "CIS205", text: "BSIS 205" }
-            ],
-            valueField: "value",
-            textField: "text"
-        },
-        {
-            type: "control"
-        }
-    ]
-}).data("JSGrid");
-
-// The minimum width of the table for responsiveness
-$('.jsgrid-table').css('min-width', '1800px');
+    error: function(data) {
+        console.log('Sections couldn\'t be retrieved');
+    }
+});
